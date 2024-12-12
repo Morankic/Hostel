417f190ae76ff059c2e7d55e2840bc7e5ee89c9e
Kenan Morankic <kmorankic1999@gmail.com>
2024-12-12 17:32:29 +0100

    dodani komentari

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-//zadatak
-// zadatak 2
+//Treba napraviti klase za usluge
 
 class Hostel{
+/* ukupan broj soba,korisnici(niz), koje su sobe dostupne*/
+//provjeri dvokretne i jednokrevetne, deluxe -ukupno 6 soba
+}
 
+class Soba {
+    // tip sobe, da li je slobodna, broj sobe , 
 }
 
 class Admin {
+//objekat, vrijeme prijave u hotel, korisnicko ime, password za korisnika ,broj i tip sobe-metoda 1(registruje)
+// promjena sobe i tipa sobe, dodatne usluge -metoda 2(dodatnaUsluga)
+//racun za usluge -metoda 3 (izdajRacun(imeKorisnika))
+//odjava korisnika i pullaj iz niza -metoda4 (odjaviKorisnika)
+//provjeri je li prijavljen-metoda 5.25 (jeLiUHotelu)
+//izloguj sve ili individualno - metoda 5.5()
+//metoda 5.75 (ugasiSistem)
+//pretrazi korisnika po imenu/br licne karte/username -metoda 6(pretraziKorisnika)
 
 }
 
 class Korisnik{
-    
-
+   //ime prezime, spol, broj licne karte, godine
+   //Koliko je duzan hotelu,koliko dana je bio,koje usluge je koristio i koliko kostaju -metoda 1
+   //rezervisi usluge - metoda 2
+   //trazi promjenu sobe -metoda 3
+   //odjava
 } 
+class Usluga{
+    //
+}
+
+class Racun{
+//ukupni racun, historija usluga
+ }
